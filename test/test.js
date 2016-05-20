ed771e18ee31943f7987795722fdaf217be00c53
Maximiliano Felice <maximilianofelice@gmail.com>
2016-05-19 22:36:08 -0300

    Adding testing with shadowing components

--- test/test.js
+++ test/test.js
@@ -1,13 +1,18 @@
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import Index from '../src/index';
+import React from 'react';
+import ReactTestUtils from 'react-addons-test-utils';
+import Element from '../src/index';
+import assert from 'assert';
 
 describe('Test', function() {
 
   chai.use(chaiAsPromised)
 
-  it('should be true', function () {
-    return chai.expect(Promise.resolve(2)).to.eventually.equal(2);
+  it('should be Element', function () {
+    const renderer = ReactTestUtils.createRenderer()
+    const result = renderer.render(<Element />);
+    return assert.equal(result.props.children, 'Element');
   });
 
 });
