4b176fc0761a9edd72ff590844f6808dc5e5b21b
Maximiliano Felice <maximilianofelice@gmail.com>
2016-06-02 12:18:34 -0300

    Adding button

---
+++ src/components/buttons.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Rx from 'rx';
+var FuncSubject = require('rx-react').FuncSubject;
+
+export default class Button extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.buttonClicked = FuncSubject.create();
+    this.buttonClicked.subscribe(this.props.onClick);
+  }
+
+  static defaultProps = {
+    onClick: event => console.log(`I was clicked, but I had no default behaviour. I'm: ${event.target}`),
+    className: "",
+    as: "default"
+  }
+
+  content() { return this.props.text || this.props.children }
+
+  render() {
+    return (
+      <button type="button" className={`btn ${this.props.className} btn-${this.props.as}`} onClick={this.buttonClicked} type={this.props.type}>
+        {this.content()}
+      </button>
+    )
+  }
+};
