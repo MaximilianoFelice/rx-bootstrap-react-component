eac9721d5bad42f4b5b662bf4b777bd84536d750
Martin Montenegro <martin.montenegro@dev.redmintlabs.com>
2016-06-02 16:00:00 -0300

    Add missing imports

--- src/components/inputs.jsx
+++ src/components/inputs.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Rx from 'rx';
 import RxReact from 'rx-react';
 import {without} from '../helpers';
+import {InputField, Label} from '../index';
 
 export default class Input extends RxReact.Component {
   static defaultProps = { observeOn: new Rx.Subject(), publishOn: new Rx.Subject() }
