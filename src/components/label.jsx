204e0fc4be7a72968562329f1726a4af1101616e
Maximiliano Felice <maximilianofelice@gmail.com>
2016-06-02 19:02:53 -0300

    Fixing Label

--- src/components/label.jsx
+++ src/components/label.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Rx from 'rx';
 import RxReact from 'rx-react';
+import {getData} from '../helpers';
 
 export default class Label extends RxReact.Component{
+  constructor(props){
+    super(props);
+    this.state = this.props;
+  }
+  static defaultProps = {observeOn: new Rx.Subject()}
 
   getStateStream(){ return this.props.observeOn.map(getData) }
 
