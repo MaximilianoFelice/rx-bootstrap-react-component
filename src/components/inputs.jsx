4aaca3e22e7e3fa25ed9914678cfa6df842a49d4
Nicolas Oga <2112.oga@gmail.com>
2016-06-10 11:18:24 -0300

    Aestethic changes to Inputfield

--- src/components/inputs.jsx
+++ src/components/inputs.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Rx from 'rx';
 import RxReact from 'rx-react';
-//import {isDefined, getData, propagable, propagableObsevable} from '../helpers';
 import {isDefined} from '../helpers';
 import Label from './label';
 import BaseComponent from "./base";
@@ -13,29 +12,28 @@ export class InputField extends BaseComponent {
     this.labelObs =
       this.props.observeOn
         .map(state => state && state.labelProps)
-        .merge(new Rx.BehaviorSubject(this.state.labelProps))
         .filter(isDefined);
 
     this.inputObs =
       this.props.observeOn
         .map(state => state && state.inputProps)
-        .merge(new Rx.BehaviorSubject(this.state.inputProps))
         .filter(isDefined);
 
     this.errorsObs =
       this.props.observeOn
         .map(state => state.errors)
-        .merge(new Rx.BehaviorSubject(this.state.errors))
         .filter(isDefined)
         .map(errors => { return {errors} })
   }
 
+  hasError() { return this.state.errors && this.state.errors.length; }
+
   render() {
     return (
-      <div>
-        <Label observeOn={this.labelObs}/>
-        <Input observeOn={this.inputObs}/>
-        <InputErrors observeOn={this.errorsObs}/>
+      <div className={`form-group ${this.hasError() && "has_error"}`}>
+        <Label observeOn={this.labelObs} {...this.state.labelProps}/>
+        <Input observeOn={this.inputObs} {...this.state.inputProps}/>
+        <InputErrors observeOn={this.errorsObs} errors={this.state.errors}/>
       </div>
     )
   }
@@ -48,10 +46,7 @@ export class Input extends BaseComponent {
 export class InputErrors extends BaseComponent {
   renderErrorMessage(str, i) {
     return (
-      <span 
-        key={`error-message-${i}`}
-        className="help-block"
-      >{str}</span>
+      <li key={`error-message-${i}`}>{str}</li>
     );
   }
 
@@ -60,6 +55,6 @@ export class InputErrors extends BaseComponent {
       isDefined(this.state.errors) &&
       this.state.errors.map(this.renderErrorMessage);
 
-    return <div>{errors}</div>;
+    return <span className="help-block"><ul>{errors}</ul></span>;
   }
 }
