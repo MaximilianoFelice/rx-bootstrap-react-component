0e7e9533ddea40775ffb43f8b125047019e066cc
Martin Montenegro <martin.montenegro@dev.redmintlabs.com>
2016-06-28 12:44:51 -0300

    Add SelectField Rebuild

--- src/components/selects.jsx
+++ src/components/selects.jsx
@@ -3,6 +3,17 @@ import Rx from 'rx';
 import RxReact from 'rx-react';
 import BaseComponent from './base';
 import {isDefined, getData, propagable, propagableObsevable} from '../helpers';
+import {UserEditableField} from "./inputs";
+
+export class SelectField extends UserEditableField {
+  renderField(){
+    return(
+      <Select observeOn={this.inputObs} {...this.state.inputProps}>
+        {this.props.children}
+      </Select>
+    )
+  }
+}
 
 export class Select extends BaseComponent {
   render(){return (
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import {Textarea, TextareaField} from './components/textareas'
 import Button from './components/buttons'
 import Modal from './components/modals'
 import Label from './components/label'
-import {Select} from './components/selects'
+import {Select, SelectField} from './components/selects'
 import helpers from './helpers'
 import BaseComponent from './components/base'
 import {Tabs, Tab} from './components/tabs'
 
 export {Input, Button, Modal, InputField, 
-        Label, Select, BaseComponent, 
+        Label, Select, SelectField, BaseComponent, 
         InputErrors, TextareaField, Textarea,
         Tabs, Tab};
